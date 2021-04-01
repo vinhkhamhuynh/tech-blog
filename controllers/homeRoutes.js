fe10a7169736ba6cf93a678170504494cd7ecc41
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-04-01 13:23:29 -0400

    created login route and render /login on html

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,29 +1,84 @@
 const router = require('express').Router();
-const {User, Post , Comment} = require('../models')
+const { User, Post, Comment } = require('../models')
 
 
 //render homepage
 router.get('/', async (req, res) => {
 
-try {
-  const postData = await Post.findAll({
-    include: [{
-      model: User,
-      attributes: ['name'],
-    }],
-  });
-  const posts = postData.map ((post) => post.get ({plain: true}));
+  try {
+    const postData = await Post.findAll({
+      include: [{
+        model: User,
+        attributes: ['name'],
+      }],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-  res.render ('homepage', {
-    posts,
-    logged_in: req.session.logged_in
-  })
-} catch (err) {
-  res.status(500).json(err);
-}
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in
+    })
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
 });
 
-//
+//get any 1 post
+router.get('/post/:id', async (req, res) => {
+ try{
+  const postData = await Post.findByPk(req.params.id, {
+    include: [
+      {
+        model: User,
+        attributes: ['name'],
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['name']
+
+        }
+      }
+    ]
+});
+
+const post = postData.get({plain: true});
+
+res.render('single-post', {
+  post, 
+  logged_in: req.session.logged_in
+
+});
+
+ }catch (err) {
+  res.status(500).json(err);
+ }
+
+});
+
+//login route
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('login');
+});
+
+//signup route
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('signup');
+
+});
+
+
 
 module.exports = router;
+
