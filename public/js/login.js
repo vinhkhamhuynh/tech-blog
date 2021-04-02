a42500de12e9fbea11740a2f15048ba06f0ab649
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-04-01 21:42:21 -0400

    created login route

--- public/js/login.js
+++ public/js/login.js
@@ -8,8 +8,8 @@ const loginFormHandler = async (event) => {
     if (email && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST',
-            body: JSON.stringify({email, password}),
-            header: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
@@ -20,7 +20,7 @@ const loginFormHandler = async (event) => {
     }
 };
 
-const signupFormHandler = async function(event) {
+const signupFormHandler = async function (event) {
     event.preventDefault();
 
 
@@ -33,11 +33,11 @@ const signupFormHandler = async function(event) {
     if (name && email && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({name, email, password}),
-            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
         });
 
-        if(response.ok) {
+        if (response.ok) {
             document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
@@ -46,9 +46,9 @@ const signupFormHandler = async function(event) {
 };
 
 document
-.querySelector('.login-form')
-.addEventListener('submit', loginFormHandler);
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
 
 document
-.querySelector('.signup-form')
-.addEventListener('submit', signupFormHandler);
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
