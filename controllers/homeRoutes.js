19f7dd60f7b700964d869fdfe54193a8294d7031
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-04-02 02:28:14 -0400

    created route for comment

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,7 +48,7 @@ router.get('/post/:id', async (req, res) => {
 
 const post = postData.get({plain: true});
 
-res.render('single-post', {
+res.render('individual-post', {
   post, 
   logged_in: req.session.logged_in
 
